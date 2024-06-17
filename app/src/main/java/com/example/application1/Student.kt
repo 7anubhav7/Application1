42c934e1e497edf438f37de013c965dc66fa7753
anubhav <122981577+anoob-leh@users.noreply.github.com>
2024-06-17 14:22:10 +0530

    added alarms with button click

--- app/src/main/java/com/example/application1/Student.kt
+++ app/src/main/java/com/example/application1/Student.kt
@@ -3,6 +3,7 @@ package com.example.application1
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -16,7 +17,17 @@ class Student : AppCompatActivity() {
     fun myClickHandler(view: View){
         Log.i("MainActivity", "button clicked")
         // var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:984532835"))
-        var webIntent:Intent=Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"))
-        startActivity(webIntent)
+        /*var webIntent:Intent=Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"))
+        startActivity(webIntent) */
+        createAlarm("vit", 19, 31)
+    }
+
+    fun createAlarm(message:String, hour:Int, minutes:Int){
+        val intent= Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE,message)
+            putExtra(AlarmClock.EXTRA_HOUR,hour)
+            putExtra(AlarmClock.EXTRA_MINUTES,minutes)
+        }
+        startActivity(intent)
     }
 }
