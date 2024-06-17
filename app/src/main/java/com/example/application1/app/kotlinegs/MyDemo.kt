a98fb95164ed937fe7d8fe6e7f54f977d3d37068
anubhav <122981577+anoob-leh@users.noreply.github.com>
2024-06-17 19:14:49 +0530

    method reference

--- app/src/main/java/com/example/application1/app/kotlinegs/MyDemo.kt
+++ app/src/main/java/com/example/application1/app/kotlinegs/MyDemo.kt
@@ -6,6 +6,8 @@ var lamda: (Int) -> Int = { a -> a*a }
 
 fun main() {
     var someLambda : (Int,Int) -> Int = {a:Int, b:Int -> a+b}
+    var someLambda2 : (Int,Int) -> Int = ::add
+    println(someLambda2(40,50))
     var lamda1:(Int,Int,String) -> Int = {a,b,c ->
         println("message is"+c)
         a*b
@@ -14,6 +16,10 @@ fun main() {
 
 fun operation(): (Int) -> Int {
     return ::square
+}
+
+fun add(a:Int,b:Int):Int{
+    return a+b
 }
 
 fun square(x: Int)= x*x
