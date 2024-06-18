b1853070768c0731f057bcb7b6792150952b4f8e
anubhav <122981577+anoob-leh@users.noreply.github.com>
2024-06-18 14:00:51 +0530

    made changes to homeactivity

--- app/src/main/java/com/example/application1/app/HomeActivity.kt
+++ app/src/main/java/com/example/application1/app/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.example.application1.app
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +12,19 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.application1.R
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    var TAG=HomeActivity::class.java.simpleName
+    lateinit var mySpinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        mySpinner =  findViewById(R.id.spinner)
+        mySpinner.onItemSelectedListener = this
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -23,6 +33,15 @@ class HomeActivity : AppCompatActivity() {
     Log.i("homeActivity", data.toString())
     var homeTextView:TextView = findViewById(R.id.textView)
     homeTextView.setText(data)
+    */
+    }
 
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var item= parent?.selectedItem.toString()
+        Log.i(TAG,item)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
