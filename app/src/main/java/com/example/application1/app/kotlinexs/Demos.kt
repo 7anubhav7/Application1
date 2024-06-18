8c91161a559721aaa57ebfdc592beec2bd79cdb8
anubhav <122981577+anoob-leh@users.noreply.github.com>
2024-06-18 07:38:36 +0530

    extension functions

--- app/src/main/java/com/example/application1/app/kotlinexs/Demos.kt
+++ app/src/main/java/com/example/application1/app/kotlinexs/Demos.kt
@@ -1,2 +1,33 @@
 package com.example.application1.app.kotlinexs
 
+fun main() {
+    println("".printVitNames("Students of vit"))
+    println(  double(2))
+    tempToday("mon",32)
+    drive(mph=18,speed="slow")
+    var a= "anubhav"
+
+
+    val numberOfDogs:Int = 3
+    val numberOfCats=2
+    println("i have $numberOfDogs of dogs and $numberOfCats of cats")
+}
+
+fun printHello(name:String?) {
+    println("Hi there!")
+}
+
+fun drive(speed:String = "fast", mph:Int): String {
+    return "driving $speed"
+}
+
+fun tempToday(day:String, temp:Int)  {
+    println("Today is  $day and it's $temp degrees")
+}
+
+fun double(x:Int) = x*2
+
+fun String.printVitNames(names:String) {
+    println(names)
+}
+
