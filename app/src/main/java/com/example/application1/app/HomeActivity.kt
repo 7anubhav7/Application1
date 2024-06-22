680512506649c1da94efda471c0a2194833f1638
anubhav <122981577+anoob-leh@users.noreply.github.com>
2024-06-22 16:35:15 +0530

    setting the parsed json into recyclerview

--- app/src/main/java/com/example/application1/app/HomeActivity.kt
+++ app/src/main/java/com/example/application1/app/HomeActivity.kt
@@ -8,21 +8,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.application1.R
+import com.example.application1.app.network.MarsAdapter
 import com.example.application1.app.network.MarsApi
+import com.example.application1.app.network.MarsPhoto
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var listMarsPhotos:List<MarsPhoto>
+    lateinit var marsAdapter: MarsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+        recyclerView  = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        listMarsPhotos = ArrayList<MarsPhoto>()
+        marsAdapter = MarsAdapter(listMarsPhotos)
+        recyclerView.adapter=marsAdapter
+
+        //brake
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //if(intent.extras is null)
     var data= intent.extras?.getString("mykey")
     Log.i("homeActivity", data.toString())
     var homeTextView:TextView = findViewById(R.id.textView)
@@ -35,8 +54,10 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun getMarsPhotos() {
-        GlobalScope.launch {
-            val listMarsPhoto = MarsApi.retrofitService.getPhotos()
+        GlobalScope.async {
+            var listMarsPhoto = MarsApi.retrofitService.getPhotos()
+            listMarsPhoto = listMarsPhotos
+            marsAdapter.notifyItemRangeChanged(0,listMarsPhoto.size)
             Log.i("HomeActivity", listMarsPhoto.get(0).imgSrc)
         }
     }
